bf96be84944c3620206a8b2e6cf9a7672cdb4060
Manan Bagga <mananbagga0310@gmail.com>
2024-09-25 20:09:29 +0530

    add UI part

---
+++ RandomPasswordGenerator/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [length, setlength] = useState(8)
+  const [numbersAllowed, setNumbersAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState('')
+
+
+  return (
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-60 bg-gray-800 text-orange-500'>
+      <h1 className='text-white text-center my-3'>Password Generator</h1>
+
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input 
+          type="text"
+          value={password}
+          className='outline-none w-full py-1 px-3'
+          placeholder='Password'
+          readOnly 
+          
+          />
+
+          <button
+          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+          >Copy</button>
+      </div>
+
+      <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input 
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className='cursor-pointer'
+            onChange={(e) => setlength(e.target.value)}
+            name='' />
+            <label htmlFor="length">Length: {length}</label>
+          </div>
+
+          <div className='flex items-center gap-x-1'>
+            <input 
+            type="checkbox"
+            defaultChecked={numbersAllowed}
+            onChange={() => {
+              setNumbersAllowed((prev) => !prev)
+            }} />
+            <label htmlFor="number">Numbers</label>
+          </div>
+
+          <div className='flex items-center gap-x-1'>
+            <input 
+            type="checkbox"
+            defaultChecked={charAllowed}
+            onChange={() => {
+              setNumbersAllowed((prev) => !prev)
+            }} />
+            <label htmlFor="charInput">Character</label>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
